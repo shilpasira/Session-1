7eb1c06c74b3f56d4547a9e600be955049944095
shilpasira <shilpa24791@gmail.com>
2016-05-10 11:05:10 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,15 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=10;
+		int y=5;
+		
+		int z=x+y;
+		
+		System.out.print("The sum of two numbers:" +z);
+	}
+
+}
