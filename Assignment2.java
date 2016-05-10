a8be3e36c03636ac61dd813e67eb7c6a0b52b92e
shilpasira <shilpa24791@gmail.com>
2016-05-10 11:07:46 +0530

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,13 @@
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Integer a,b=4,c;
+		
+		System.out.println("The value of b is:" +b);
+		
+	}
+
+}
