1172eaebafc18c6995c2a1476383718fb26c77e5
shilpasira <shilpa24791@gmail.com>
2016-05-10 11:10:18 +0530

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,16 @@
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double a=(float) 2;
+		float b=(float) 3;
+		
+		double z=a+b;
+		System.out.println(z);
+		
+		
+	}
+
+}
